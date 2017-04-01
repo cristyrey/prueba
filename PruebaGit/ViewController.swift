694026c2d515ed90741e779dbbfadc599c0d6e75
Cristina Rey <cristinarey@MacBook-Pro-de-Cristina.local>
2017-04-01 00:48:55 -0400

    Initial Commit

---
+++ PruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGit
+//
+//  Created by Cristina Rey on 4/1/17.
+//  Copyright © 2017 Cristina Rey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
